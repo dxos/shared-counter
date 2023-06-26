f3a7db9c7cb8946672a9eef22a00b17228f5e9b5
Jess Martin <jessmartin@gmail.com>
2023-06-26 10:39:03 -0400

    Update to match docs

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,34 +1,32 @@
-import { Expando, useIdentity, useQuery, useSpaces } from "@dxos/react-client";
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
+import { Expando, useIdentity, useQuery, useSpaces } from '@dxos/react-client';
 
 export const Counter = () => {
-  const identity = useIdentity({ login: true });
+  useIdentity({ login: true });
   const [space] = useSpaces();
-  const [counter] = useQuery(space, { type: "counter" });
+  const [counter] = useQuery(space, { type: 'counter' });
 
   useEffect(() => {
     if (space && !counter) {
-      const c = new Expando({ type: "counter", values: [] });
-      space.db.add(c);
+      const counter = new Expando({ type: 'counter', values: [] });
+      space.db.add(counter);
     }
   }, [space, counter]);
 
   return (
     <div>
-      {counter ? (
-        <div className="text-center">
+      {counter && (
+        <div className='text-center'>
           <button
-            className="border bg-white dark:bg:black py-2 px-4 my-2 rounded hover:bg-gray-100"
+            className='border bg-white py-2 px-4 my-2 rounded hover:bg-gray-100'
             onClick={() => {
               counter.values.push(1);
             }}
           >
             Click me
           </button>
-          <p>Clicked {counter.values ? counter.values.length : "0"} times.</p>
+          <p>Clicked {counter.values.length ?? 0} times.</p>
         </div>
-      ) : (
-        <div className="text-center">No counter created.</div>
       )}
     </div>
   );
